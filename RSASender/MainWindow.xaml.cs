21cf1f5a59cd1ecd296945ba088ca94768a25e13
Your Name <flemming@northtech.dk>
2021-01-07 13:39:37 +0100

    Initial Commit

---
+++ RSASender/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RSASender
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private RSAEncrypt _rsa;
+        public MainWindow()
+        {
+            InitializeComponent();
+            _rsa = new RSAEncrypt();
+            _rsa.AssignNewKey();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(ModulusText.Text))
+            {
+                MessageBox.Show("You must enter Modulus");
+            }
+            else
+            {
+                _rsa._publicKey.Modulus = Convert.FromBase64String(ModulusText.Text);
+                _rsa._publicKey.Exponent = Convert.FromBase64String(ExponentText.Text);
+                byte[] encryptedMessege = _rsa.EncryptData(Encoding.UTF8.GetBytes(MessageText.Text));
+                //byte[] encryptedMessege2 = _rsa.EncryptData(Convert.FromBase64String(MessageText.Text));
+                CipherBytesText.Text = Convert.ToBase64String(encryptedMessege);
+                //CipherBytesText.Text = Convert.ToBase64String(encryptedMessege2);
+                //byte[] DecryptedMessage = _rsa.EncryptData(encryptedMessege);
+                //DecryptedText.Text = Convert.ToBase64String(_rsa.DecryptData(encryptedMessege));
+            }
+        }
+    }
+}
---
+++ RSASender/RSAEncrypt.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace RSASender
+{
+    class RSAEncrypt
+    {
+        public RSAParameters _publicKey;
+        public RSAParameters _privateKey;
+
+        public void AssignNewKey()
+        {
+            using (var rsa = new RSACryptoServiceProvider(2048))
+            {
+                rsa.PersistKeyInCsp = false;
+                _publicKey = rsa.ExportParameters(false);
+                _privateKey = rsa.ExportParameters(true);
+            }
+        }
+
+        public byte[] EncryptData(byte[] dataToEncrypt)
+        {
+            byte[] cipherbytes;
+
+            using (var rsa = new RSACryptoServiceProvider(2048))
+            {
+                rsa.PersistKeyInCsp = false;
+                rsa.ImportParameters(_publicKey);
+
+                cipherbytes = rsa.Encrypt(dataToEncrypt, true);
+            }
+
+            return cipherbytes;
+        }
+
+        public byte[] DecryptData(byte[] dataToEncrypt)
+        {
+            byte[] plain;
+
+            using (var rsa = new RSACryptoServiceProvider(2048))
+            {
+                rsa.PersistKeyInCsp = false;
+
+                rsa.ImportParameters(_privateKey);
+                plain = rsa.Decrypt(dataToEncrypt, true);
+            }
+
+            return plain;
+        }
+    }
+}
+
