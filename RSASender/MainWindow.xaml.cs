dd86383ba0ca542cd9d714a7d33de1eb40551a50
Your Name <flemming@northtech.dk>
2021-01-07 19:15:03 +0100

    Server Client Solution

--- RSASender/MainWindow.xaml.cs
+++ RSASender/MainWindow.xaml.cs
@@ -22,11 +22,33 @@ namespace RSASender
     {
         private readonly RSAEncrypt _rsa;
 
+        delegate void UpdateCipherText(string cipher);
+
+        private readonly TcpServer _tcpServer;
+        const int PortNo = 13005;
+        const string ServerIp = "127.0.0.1";
+
         public MainWindow()
         {
             InitializeComponent();
+            // Here i create a tcp server
+            _tcpServer = new TcpServer();
+            // here i Start the tcp server
+            Task.Run((() => _tcpServer.TcpServerStart(ServerIp, PortNo)));
+            // here i subscribe to the event when message was received
+            _tcpServer.MessageReceived += _tcpServer_MessageReceived;
             _rsa = new RSAEncrypt();
             _rsa.AssignNewKey();
+        }
+
+        private void _tcpServer_MessageReceived(object sender, string e)
+        {
+            Dispatcher.BeginInvoke(new UpdateCipherText(UpdateCipher), new object[] {e});
+        }
+
+        private void UpdateCipher(string cipher)
+        {
+            ModulusText.Text = cipher;
         }
 
         /// <summary>
@@ -45,11 +67,14 @@ namespace RSASender
                 // updates this Pyblic key so that it get Receivers public key
                 _rsa._publicKey.Modulus = Convert.FromBase64String(ModulusText.Text);
                 _rsa._publicKey.Exponent = Convert.FromBase64String(ExponentText.Text);
-                
+
                 // Encrypts the message 
                 byte[] encryptedMessage = _rsa.EncryptData(Encoding.UTF8.GetBytes(MessageText.Text));
                 // Displays the text in the window
-                CipherBytesText.Text = Convert.ToBase64String(encryptedMessage);
+                string message = Convert.ToBase64String(encryptedMessage);
+                CipherBytesText.Text = message;
+                TcpClient client = new TcpClient();
+                client.Connect("127.0.0.1", message);
             }
         }
     }
---
+++ RSASender/TcpServer.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Windows;
+
+namespace RSASender
+{
+    class TcpServer
+    {
+        public event EventHandler<string> MessageReceived;
+
+        public void TcpServerStart(string serverIp, int portNo)
+        {
+            TcpListener server = null;
+            try
+            {
+                // Set the TcpListener on port 13000.
+                Int32 port = 13005;
+                IPAddress localAddr = IPAddress.Parse("127.0.0.1");
+
+                // TcpListener server = new TcpListener(port);
+                server = new TcpListener(localAddr, port);
+
+                // Start listening for client requests.
+                server.Start();
+
+                // Buffer for reading data
+                Byte[] bytes = new Byte[512];
+                String data = null;
+
+                // Enter the listening loop.
+                while (true)
+                {
+                    // Perform a blocking call to accept requests.
+                    // You could also use server.AcceptSocket() here.
+                    System.Net.Sockets.TcpClient client = server.AcceptTcpClient();
+                    
+                    data = null;
+
+                    // Get a stream object for reading and writing
+                    NetworkStream stream = client.GetStream();
+
+                    int i;
+
+                    // Loop to receive all the data sent by the client.
+                    while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
+                    {
+                        // Translate data bytes to a ASCII string.
+                        data = System.Text.Encoding.UTF8.GetString(bytes, 0, i);
+                        MessageReceived?.Invoke(this, data);
+
+                        // Process the data sent by the client.
+                        data = data.ToUpper();
+
+                        byte[] msg = Encoding.UTF8.GetBytes(data);
+
+                        // Send back a response.
+                        stream.Write(msg, 0, msg.Length);
+                       
+                    }
+
+                    // Shutdown and end connection
+                    client.Close();
+                }
+            }
+            catch (SocketException e)
+            {
+                MessageBox.Show($"{e}");
+            }
+            finally
+            {
+                // Stop listening for new clients.
+                server.Stop();
+            }
+        }
+    }
+}
